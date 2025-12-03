e465bfbeba384afbff28059f490ef984ce63f39b
TyAltern <tyalternative3333@gmail.com>
2025-12-03 19:48:58 +0100

    added Custom Items/Blocks/Categories

--- src/main/java/net/tyalternative/tutorialmod/TutorialMod.java
+++ src/main/java/net/tyalternative/tutorialmod/TutorialMod.java
@@ -2,15 +2,21 @@ package net.tyalternative.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.tyalternative.tutorialmod.block.ModBlocks;
+import net.tyalternative.tutorialmod.item.ModItemGroups;
+import net.tyalternative.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // added comment
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroup();
 	}
 }
